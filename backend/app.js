931b2df9209ca2fe74533db36e0af44d9d7a03ec
PrathamSingh1 <prathamjii36@gmail.com>
2025-06-27 19:06:56 +0000

    create basic server

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 3000;
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+module.exports = app;
