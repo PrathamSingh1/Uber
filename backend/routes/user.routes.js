8c6e94617f80d5d6404822e5f8dbf483b409f37b
PrathamSingh1 <prathamjii36@gmail.com>
2025-06-28 11:14:58 +0000

    login route done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,7 +9,15 @@ router.post('/register', [
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ],
-userController.registerUser
+    userController.registerUser
+)
+
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password Invalid')
+],
+    userController.loginUser
 )
 
 
